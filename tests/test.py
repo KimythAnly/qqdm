53842f857c8e04942f6140d88d6eee6a074e7da6
anlycchen <kimythanly@gmail.com>
2021-02-01 22:33:38 +0800

    Add demo.gif

--- tests/test.py
+++ tests/test.py
@@ -6,8 +6,8 @@ from qqdm import qqdm, format_str
 start = time.time()
 
 for ep in range(2):
-    tw = qqdm(range(10000), desc=format_str('blue', f'asdadsfd'))
-    time.sleep(3)
+    tw = qqdm(range(100), desc=format_str('blue', f'asdadsfd'))
+    time.sleep(1)
     for i in tw:
         tw.set_infos({
             'loss': f'{random.random():.4f}',
