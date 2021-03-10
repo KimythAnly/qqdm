adbee4bc357085760b8f069e2d06f79df4902d8f
anlycchen <kimythanly@gmail.com>
2021-03-10 20:48:28 +0800

    0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="qqdm",
-    version="0.0.4",
+    version="0.0.5",
     author="KimythAnly",
     author_email="kimythanly@gmail.com",
     description="A very lightweight progress bar.",
